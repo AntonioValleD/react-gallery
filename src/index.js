8bc9c9e2e3180d05c6ad45d715ef27865bc97bc2
AntonioValleD <juan.valle.d1@gmail.com>
2023-09-16 11:09:27 -0600

    index.js restructured

--- src/index.js
+++ src/index.js
@@ -8,11 +8,11 @@ import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider store={store}>
-    <React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
       <HashRouter>
         <App />
       </HashRouter>
-    </React.StrictMode>
-  </Provider>
+    </Provider>
+  </React.StrictMode>
 );
